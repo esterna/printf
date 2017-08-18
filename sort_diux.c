3b6584763dc5e37ee2f62ebadf820b9d12df414a
Eliana Sterna <emsterna@yahoo.com>
2017-08-17 19:48:54 -0700

    Test

--- sort_diux.c
+++ sort_diux.c
@@ -6,7 +6,7 @@
 /*   By: esterna <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/16 17:30:32 by esterna           #+#    #+#             */
-/*   Updated: 2017/08/17 18:22:48 by esterna          ###   ########.fr       */
+/*   Updated: 2017/08/17 19:27:15 by esterna          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -126,8 +126,7 @@ char			*sort_d(t_format format, va_list arg)
 	dbl = ft_round_dbl(dbl, format.precision);
 	if (format.specifier == 'G' || format.specifier == 'g')
 		tmp = sort_g(format, dbl, base);
-	else if (format.specifier != 'F' && format.specifier != 'f'
-			&& format.specifier != 'A' && format.specifier != 'a')
+	else if (format.specifier != 'F' && format.specifier != 'f')
 		tmp = ft_dtosf_base(dbl, base, format.precision);
 	else
 		tmp = ft_dtoa_base(dbl, base, format.precision);
