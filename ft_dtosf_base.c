d01aebe1477690b6af812ec1bb97b47da4dd6fc6
Eliana Sterna <emsterna@yahoo.com>
2017-08-17 21:30:50 -0700

    Test

--- ft_dtosf_base.c
+++ ft_dtosf_base.c
@@ -6,7 +6,7 @@
 /*   By: esterna <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/13 13:47:49 by esterna           #+#    #+#             */
-/*   Updated: 2017/08/17 21:29:48 by esterna          ###   ########.fr       */
+/*   Updated: 2017/08/17 21:30:48 by esterna          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,7 +74,7 @@ static char			*str_setup(double dbl, int base, int precision)
 
 	exp = find_exponent(dbl, base);
 	len = 4 + ((precision > 0) ? precision + 1 : 0) + ((dbl < 0) ? 1 : 0)
-		+ ((base == 16) ? 2 : 0) + ((exp > 9) ? 1 : 0);
+		+ ((base == 16) ? 2 : 0) + ((base == 10) ? 1 : 0) + ((exp > 9) ? 1 : 0);
 	sf = ft_strnew(sizeof(char) * len);
 	*sf = (dbl < 0.0) ? '-' : '\0';
 	if (base == 10 && precision != 0)
